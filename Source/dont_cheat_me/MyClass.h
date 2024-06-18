df1a99ce009bf8bac1702cba66e0ad3e584197b9
Gaspard <tapongaspard16@gmail.com>
2024-06-18 19:48:06 +0200

    Menu finish

---
+++ Source/dont_cheat_me/MyClass.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class DONT_CHEAT_ME_API MyClass
+{
+public:
+	MyClass();
+	~MyClass();
+};
---
+++ Source/dont_cheat_me/dont_cheat_me.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "dont_cheat_me.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, dont_cheat_me, "dont_cheat_me" );
